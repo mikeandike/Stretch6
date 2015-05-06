b0edb87de41648ad43de60c69ca7d2d74063b45d
Michael Sacks <michaelsacks@gmail.com>
2015-05-06 09:30:21 -0600

    Initial Commit

---
+++ Stretch6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stretch6
+//
+//  Created by Michael Sacks on 5/6/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
